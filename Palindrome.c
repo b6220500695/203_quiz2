6ccb7ea7eacbb099834d15e332be8b9ec34ee5b3
mmint.m <pheeraya.p@ku.th>
2021-03-25 17:59:33 +0700

    1

--- Palindrome.c
+++ Palindrome.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-   int i,par=2,len ;
    char a[200],b[200];
+   int i,par=2,len ;
    scanf("%d",&a);
    for(i=1;a[i-1]!=0;i++)
       len++;
@@ -20,7 +20,16 @@ int main()
    }
   if(par!=0)
   {
-
+     for(i=1;i<=(len%2)/2;i++)
+     {
+       if(a[i-1]!=b[len%2/2+len%2+i-1])
+       par = 1;
+     }
+     for(i=1;i<=(len%2)/2;i++)
+     {
+       if(a[len/2+len%2+i-1]!=b[i-1])
+       par = 2;
+     }
   }
 
 
