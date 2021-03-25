984876d4c4feaff9e2f178f70f4724b5b2b61467
mmint.m <pheeraya.p@ku.th>
2021-03-25 19:44:00 +0700

    1

--- Cube Free Numbers.c
+++ Cube Free Numbers.c
@@ -3,16 +3,21 @@ int main()
 {
    int n,a,b,c ;
    scanf("%d",&n);
-
    if((1<=n)&&(n<=1000000))
    {
-      a=n/2;
-      b=n%2;
-      c=b;
-      printf("%d\n",c);
+     a=n/2;
+     b=n%2;
+     c=b;
+      //printf("%d\n",c);
      for(a=1;a<=n;a++)
      {
+        for(a=1;a<=n;a++)
+        {
+
+        }
+        printf("%d",n);
      }
-   printf("%d",n);
+
    }
+   return 0;
 }
