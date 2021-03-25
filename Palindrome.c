7a26069c40de5c242048c1ff6a8a62273a6780a3
mmint.m <pheeraya.p@ku.th>
2021-03-25 17:39:48 +0700

    มั่ว55

---
+++ Palindrome.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+   int a,i,par,len ;
+   scanf("%d",&a);
+   for(i=1;a[i-1]!=0;i++)
+   {
+      for(i=1;i<=len;i++){
+         if(a[i-1]>90&&a[i-1]>99)
+         {
+            a[i-1]-32;
+            b[len-i]=a[i-1];
+         }
+   }}
+   for(i=1;i<=len;i++)
+   {
+
+   }
+
+
+
+}
