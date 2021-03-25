7c750135380a836df7e2b5e69f5fb2ced81ca20d
mmint.m <pheeraya.p@ku.th>
2021-03-25 18:18:11 +0700

    1

--- Palindrome.c
+++ Palindrome.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-   char a[201],b[200];
-   int i,par=2,len ;
+   char a[201],b[201];
+   int i,par=2,len;
    scanf("%s",&a);
    for(i=1;a[i-1]!=0;i++)
       len++;
       for(i=1;i<=len;i++){
-         if(a[i-1]>1&&a[i-1]<=99)
+         if(a[i-1]>97&&a[i-1]<=99)
          {
             a[i-1]=a[i-1]-32;
             b[len-i]=a[i-1];
@@ -28,7 +28,7 @@ int main()
      for(i=1;i<=(len%2)/2;i++)
      {
        if(a[len/2+len%2+i-1]!=b[i-1])
-       par =2;
+       par =1;
      }
   }
   if(par==0)
@@ -41,7 +41,7 @@ int main()
   }
   if(par==2)
   {
-     printf("2");
+   printf("2");
   }
   return 0 ;
 }
